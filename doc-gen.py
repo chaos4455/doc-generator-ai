7542cb089bdf2c00a2a12f2e6c3bb5d19940ccba
Elias dos Santos de Andrade <30844283+chaos4455@users.noreply.github.com>
2025-08-15 13:52:46 -0300

    Update doc-gen.py

--- doc-gen.py
+++ doc-gen.py
@@ -31,7 +31,7 @@ def generate_content(prompt_text):
         ]
     }
 
-    url = f'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={API_KEY}'
+    url = f'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}'
 
     response = requests.post(url, json=content)
 
